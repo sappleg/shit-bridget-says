e18c079b5374ac485d696ec1d4198d6d10d27c93
Nebuleaf <alan@nebuleaf.com>
2013-01-15 01:47:44 -0500

    done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,35 @@
 'use strict';
 
 /* Controllers */
+var myApp = angular.module('myApp', []);
+
+function mainCtrl($scope, $http) {
+    $scope.tweets = [];
+    $scope.activeTweet = {};
+    var numTweets = 20;
+    var counter = 0;
+
+    $scope.nextTweet = function () {
+        counter++;
+        if(counter !== numTweets) {
+            $scope.activeTweet = $scope.tweets[counter];
+        } else {
+           counter = 0;
+           $scope.activeTweet = $scope.tweets[counter];
+        }
 
 
-function mainCtrl($scope) {
+    };
 
+    (function () {
+        $http.jsonp('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=BSlowAndCo&count=' + numTweets + '&callback=JSON_CALLBACK').
+            success(function(data) {
+                angular.copy(data, $scope.tweets);
+                $scope.activeTweet = $scope.tweets[counter];
+                console.log($scope.tweets);
+            }).error(function(data){
+                console.log("Error");
+            });
+    })();
 }
-MyCtrl1.$inject = [$scope];
 
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
